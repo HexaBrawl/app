e43a7269ead6e57ec6e1a1f7f9fb56cb5b71d520
anjaal <anjaal@edu.aau.at>
2026-04-08 13:01:15 +0200

    Integrated app logo into project and display on HomeScreen

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/lobby/HomeScreen.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/lobby/HomeScreen.kt
@@ -1,11 +1,13 @@
 package at.aau.serg.websocketbrokerdemo.ui.lobby
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -14,9 +16,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.myapplication.R
 
 @Composable
 fun HomeScreen(navController: NavController) {
@@ -36,6 +40,15 @@ fun HomeScreen(navController: NavController) {
                 .align(Alignment.TopCenter)
                 .padding(top = 64.dp)
         )
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Image(painter = painterResource(id = R.drawable.logo),
+            contentDescription = "App Logo",
+            modifier = Modifier
+                .align(Alignment.Center)
+                .padding(bottom = 370.dp)
+                .size(250.dp)
+        )
 
         TextButton(onClick = { /* Settings */ },
             modifier = Modifier
@@ -52,7 +65,7 @@ fun HomeScreen(navController: NavController) {
         Button(onClick = { navController.navigate("game") },
             modifier = Modifier.align(Alignment.Center))
         {
-            Text("PLAY", fontSize = 24.sp)
+            Text("PLAY", fontSize = 30.sp)
         }
 
         TextButton(onClick = { /* Exit App */ },
