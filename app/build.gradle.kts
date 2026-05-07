beeacd2804ecd94a183e79900f36d076b102bf0b
nihobel <nihobek@edu.aau.at>
2026-05-07 23:21:25 +0200

    Addded new tests for SettingsRepository

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -137,8 +137,10 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.junit.jupiter.api)
     testImplementation(libs.mockk)
+    androidTestImplementation(libs.junit.jupiter)
     testRuntimeOnly(libs.junit.jupiter.engine)
     testRuntimeOnly(libs.junit.platform.launcher)
+    testImplementation(libs.kotlinx.coroutines.test)
 
     // Instrumented Tests
     androidTestImplementation(libs.androidx.junit)
