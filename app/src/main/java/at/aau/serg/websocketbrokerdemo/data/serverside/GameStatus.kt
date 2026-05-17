af79b903a36d389187624a9d44bbf7f329ed3352
ManKotzent <137501602+ManKotzent@users.noreply.github.com>
2026-05-17 16:53:51 +0200

    Created on image of serverside data classes

---
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/data/serverside/GameStatus.kt
@@ -0,0 +1,8 @@
+package at.aau.serg.websocketbrokerdemo.data.serverside
+
+//As per at.aau.hexabrawl.websocketserver.model
+enum class GameStatus {
+    WAITING_FOR_PLAYERS,
+    IN_PROGRESS,
+    FINISHED
+}
---
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/data/serverside/PlayerColor.kt
@@ -0,0 +1,6 @@
+package at.aau.serg.websocketbrokerdemo.data.serverside
+
+//As per at.aau.hexabrawl.websocketserver.model
+enum class PlayerColor {
+    RED,BLUE
+}
