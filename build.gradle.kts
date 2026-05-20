8d5315b07f25738bb9b63f784ef0aae501425275
Stefan <stefan.l2k2@gmail.com>
2026-05-20 17:00:47 +0200

    changed sonarquebe version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,7 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
-    //noinspection NewerVersionAvailable - Do not mark this as a warning
-    id("org.sonarqube") version "5.1.0.4882"
+    id("org.sonarqube") version "6.0.1.5171"
 }
 
 sonar {
