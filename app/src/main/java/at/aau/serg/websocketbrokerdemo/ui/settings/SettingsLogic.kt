21342019b31e59836cccc1e0b92bb870c1ab2039
anjaal <anjaal@edu.aau.at>
2026-05-31 19:29:58 +0200

    Updated SettingsLogic and MainActivity due to renaming of other klasses for better understanding

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/settings/SettingsLogic.kt
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/settings/SettingsLogic.kt
@@ -3,13 +3,13 @@ package at.aau.serg.websocketbrokerdemo.ui.settings
 import android.app.Activity
 import android.app.Application
 import at.aau.serg.websocketbrokerdemo.audio.MusicManager
-import at.aau.serg.websocketbrokerdemo.data.LocaleCache
+import at.aau.serg.websocketbrokerdemo.data.LanguageCache
 
 class SettingsLogic(private val app: Application) {
 
     fun changeLanguage(currentLang: String, newLang: String, activity: Activity?) {
         if (currentLang == newLang) return
-        LocaleCache.set(app, newLang)
+        LanguageCache.set(app, newLang)
         activity?.recreate()
     }
 
