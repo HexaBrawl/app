b4a7cbdce86e419f4c23b7490518d6a12fe6c5ab
anjaal <anjaal@edu.aau.at>
2026-06-03 19:48:58 +0200

    Update Sonar exclusions after game-package refactoring

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,10 @@ sonar {
             listOf(
                 // Composables / UI-Screens
                 "app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/components/**",
+                "app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/game/components/**",
                 "app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/game/GameScreen.kt",
+                "app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/game/GameDebugConfig.kt",
+                "app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/game/camera/CameraModifier.kt",
                 "app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/home/HomeScreen.kt",
                 "app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/lobby_modes/ActionCard.kt",
                 "app/src/main/java/at/aau/serg/websocketbrokerdemo/ui/lobby_modes/JoinByCodeDialog.kt",
