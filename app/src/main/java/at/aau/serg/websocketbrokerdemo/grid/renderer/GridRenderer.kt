d36c906818c68624f59b2f2bb3ad9ef5fed52f7d
anjaal <anjaal@edu.aau.at>
2026-06-03 23:49:52 +0200

    Introduce single HexRenderer class and removing the old 4-class renderer chain

--- app/src/main/java/at/aau/serg/websocketbrokerdemo/grid/renderer/GridRenderer.kt
+++
@@ -1,8 +0,0 @@
-package at.aau.serg.websocketbrokerdemo.grid.renderer
-
-import androidx.compose.ui.graphics.drawscope.DrawScope
-import at.aau.serg.websocketbrokerdemo.grid.model.GridModel
-
-fun interface GridRenderer {
-    fun DrawScope.render(model: GridModel)
-}
