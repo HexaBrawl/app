97b29cc8cb7ff8f715cb303abf220d9db7cbf148
ManKotzent <137501602+ManKotzent@users.noreply.github.com>
2026-06-04 13:10:13 +0200

    Added UnitIconProvider.kt for managing unit icon drawables

---
+++ app/src/main/java/at/aau/serg/websocketbrokerdemo/grid/UnitIconProvider.kt
@@ -0,0 +1,47 @@
+package at.aau.serg.websocketbrokerdemo.grid
+
+import com.example.myapplication.R
+import at.aau.serg.websocketbrokerdemo.data.serverside.PlayerColor
+import at.aau.serg.websocketbrokerdemo.data.serverside.UnitType
+
+/**
+ * Provides drawable resource IDs for game units and castles based on player color and unit type.
+ */
+object UnitIconProvider {
+
+    /**
+     * Returns the drawable resource ID for a given unit type and player color.
+     * Skeletons use the same icon regardless of color.
+     */
+    fun iconFor(color: PlayerColor, type: UnitType): Int = when (type) {
+        UnitType.SKELETON -> R.drawable.figure_skeleton
+        UnitType.INFANTRY -> when (color) {
+            PlayerColor.RED -> R.drawable.figure_red_infantry
+            PlayerColor.BLUE -> R.drawable.figure_blue_infantry
+            PlayerColor.GREEN -> R.drawable.figure_green_infantry
+            PlayerColor.YELLOW -> R.drawable.figure_yellow_infantry
+        }
+        UnitType.CAVALRY -> when (color) {
+            PlayerColor.RED -> R.drawable.figure_red_cavalry
+            PlayerColor.BLUE -> R.drawable.figure_blue_cavalry
+            PlayerColor.GREEN -> R.drawable.figure_green_cavalry
+            PlayerColor.YELLOW -> R.drawable.figure_yellow_cavalry
+        }
+        UnitType.ARCHER -> when (color) {
+            PlayerColor.RED -> R.drawable.figure_red_archer
+            PlayerColor.BLUE -> R.drawable.figure_blue_archer
+            PlayerColor.GREEN -> R.drawable.figure_green_archer
+            PlayerColor.YELLOW -> R.drawable.figure_yellow_archer
+        }
+    }
+
+    /**
+     * Returns the drawable resource ID for a castle of the given player color.
+     */
+    fun castleFor(color: PlayerColor): Int = when (color) {
+        PlayerColor.RED -> R.drawable.castle_red
+        PlayerColor.BLUE -> R.drawable.castle_blue
+        PlayerColor.GREEN -> R.drawable.castle_green
+        PlayerColor.YELLOW -> R.drawable.castle_yellow
+    }
+}
